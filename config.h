d222f29f7bf402de24c79c06ba535a0e61558bac
zhby1990 <914793713@qq.com>
2012-03-05 05:41:02 +0800

    3

---
+++ config.h
@@ -0,0 +1,16 @@
+#define WAITING 0
+#define COMPLIERING 10
+#define RUNNING 9
+#define CE 8
+#define WA 5
+#define TLE 3
+#define OLE 7
+#define RE 6
+#define PE 2
+#define AC 1
+#define SYSTEM_ERR 11
+#define MLE 4
+#define RF 12 
+#define STATUS_SIG 7
+#define STATUS_AC 8
+
--- main.cpp
+++ main.cpp
@@ -1,1 +1,34 @@
-ssadas
+ #include<stdlib.h>
+ #include<mysql.h>
+#include<stdio.h>
+#define host "localhost"
+#define user "root"
+#define password ""
+#define database "judgeonline"
+MYSQL my_connection;
+typedef struct {
+	int solution_id;
+	char user_id[30];
+	int problem_id,contest_id;
+	double time,memory;
+	double stime,ntime;
+	double run_t,run_m;
+	int language,result,num;
+}Submits;
+
+static bool dbconnected=false;
+inline void mysql_connection()
+{
+    if(dbconnected)return;
+    mysql_init(&my_connection);
+    if(mysql_real_connect(&my_connection,host,user,password,database,0,NULL,0))
+    {
+	dbconnected=true;
+	printf("connection success\n");
+    }
+    else printf("connection faild\n");
+}
+int main()
+{
+    mysql_connection();
+}
